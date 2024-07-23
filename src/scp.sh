911ecd264a708e0a7118f490223f7895c2ede089
gerhead <you@example.com>
2024-07-23 19:33:25 +0500

    final commit

---
+++ src/scp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp ../artifacts/s21_cat ../artifacts/s21_grep root@172.24.116.8:/usr/local/bin
---
+++ src/tg_notifications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+token_bot="6759352631:AAHPtHocoYIjnl1LSj7wDln5Sanqmyp1PqE"
+TELEGRAM_BOT_TOKEN="6759352631:AAHPtHocoYIjnl1LSj7wDln5Sanqmyp1PqE"
+TELEGRAM_USER_ID="458979666"
+TIME=8
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="PROJECT: $1%0A%0AProject:+$CI_PROJECT_NAME%0AJOB_STAGE:$CI_JOB_STAGE
+%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ASTATUS:$CI_JOB_STATUS"
+
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
